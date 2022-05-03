740ca858241c1166ed8b2911ea8b3fecd6d2f11b
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-05-02 19:41:12 -0700

    Start work on lexer

--- src/Lexer.lua
+++ src/Lexer.lua
@@ -34,11 +34,43 @@ Lexer.Reserved = {
 	["while"] = Token.Kind.ReservedWhile,
 }
 
+Lexer.Operators = {
+	["+"] = Token.Kind.Plus,
+	["-"] = Token.Kind.Minus,
+	["*"] = Token.Kind.Star,
+	["/"] = Token.Kind.Slash,
+	["%"] = Token.Kind.Modulo,
+	["#"] = Token.Kind.Hashtag,
+	["?"] = Token.Kind.QuestionMark,
+
+	["^"] = Token.Kind.Caret,
+	[";"] = Token.Kind.SemiColon,
+	[":"] = Token.Kind.Colon,
+	["."] = Token.Kind.Dot,
+	[".."] = Token.Kind.Dot2,
+	["..."] = Token.Kind.Dot3,
+
+	["~="] = Token.Kind.NotEqual,
+	["="] = Token.Kind.Equal,
+	["<"] = Token.Kind.LessThan,
+	["<="] = Token.Kind.LessEqual,
+	[">"] = Token.Kind.GreaterThan,
+	[">="] =Token.Kind.GreaterEqual,
+
+	["("] = Token.Kind.LeftParen,
+	[")"] = Token.Kind.RightBracket,
+	["["] = Token.Kind.LeftBracket,
+	["]"] = Token.Kind.RightBracket,
+	["{"] = Token.Kind.LeftBrace,
+	["}"] = Token.Kind.RightBrace,
+}
+
 function Lexer.new(source)
 	local self = {}
 	setmetatable(self, Lexer)
 
 	self._source = source
+	self._position = 1
 	self._tokens = {}
 
 	return self
@@ -46,6 +78,32 @@ end
 
 function Lexer.is(object)
 	return type(object) == "table" and getmetatable(object) == Lexer
+end
+
+--[[
+	Peeks at the next character without actually consuming it.
+]]
+function Lexer:_peek(lookAhead)
+	local position = self._position + (lookAhead or 0)
+	return self._source:sub(position, position)
+end
+
+--[[
+	Consumes a character and returns what it was.
+]]
+function Lexer:_consume(count)
+	self._position = self._position + (count == nil and 1 or count)
+end
+
+function Lexer:readComment()
+	if self:_accept("--") then
+		-- Block comments can just be parsed as strings.
+		if self:_peek("[") then
+			return self:readLongString()
+		end
+
+
+	end
 end
 
 return Lexer
